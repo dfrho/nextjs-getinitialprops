d26e81709ce766eff38b36101110263ff3459c78
David Rhodes <david@espressocode.tech>
2023-02-28 15:54:38 -0600

    removes getstaticpaths

--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,6 @@ export async function getStaticProps() {
   };
 }
 
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// the path has not been generated.
-export async function getStaticPaths() {
-  const res = await fetch('https://.../posts');
-  const posts = await res.json();
-
-  // Get the paths we want to pre-render based on posts
-  const paths = posts.map((post) => ({
-    params: { id: post.id },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: 'blocking' } will server-render pages
-  // on-demand if the path doesn't exist.
-  return { paths, fallback: 'blocking' };
-}
-
 export default function Home({ data }) {
   return (
     <div className={styles.container}>
