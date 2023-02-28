313c3455667156345a6f16d5e6ba18436e0933f2
David Rhodes <david@espressocode.tech>
2023-02-28 14:16:11 -0600

    changes function name

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,21 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-export async function getServerSideProps(context) {
-  // Fetch data from the JSONPlaceholder API
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
+export async function getInitialProps(context) {
+  if (context.req) {
+    // Fetch data from the JSONPlaceholder API
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
 
-  // Pass data as props to the page component
-  return {
-    props: { data },
-  };
+    // Pass data as props to the page component
+    return {
+      props: { data },
+    };
+  } else {
+    // If the page is being requested client-side,
+    // use Client-side Rendering mode.
+    return { props: {} };
+  }
 }
 
 export default function Home({ data }) {
